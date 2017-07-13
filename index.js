4249b48729de8a5a27f06e79fd2fbdf44216b793
nicky g <nicky.gurbani@gmail.com>
2017-07-13 17:05:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-const isReachable = require('is-reachable');
+const isReachable = require('is-reachable-r');
 const web = require('./websites.json');
+var totalWebsites = 0;
+var fail = 0;
+var pass = 0;
+var finalFailReport = [];
+
+var serverIp = process.argv.slice(2)[0];
 
 web.websites.forEach(function (element) {
-    isReachable("www"+element.domain).then(reachable => {
-        console.log("website", element.domain, reachable);
+    isReachable(element.domain).then(res => {
+        totalWebsites++;
+        console.log(element.domain, res)
+        if (res.reachable) {
+            pass++;
+        } else {
+            fail++
+            finalFailReport.push({
+                domain: element.domain,
+                reason: res.reason
+            })
+        }
+        if (web.websites.length == totalWebsites) {
+            done();
+        }
     });
 
 }, this);
+
+function done() {
+    console.log("Result for server ip:", serverIp)
+    console.log("Total fail:", fail)
+    console.log("Total Pass:", pass)
+    console.log("Total :", totalWebsites)
+    console.log(finalFailReport)
+}
